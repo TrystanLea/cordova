43cff73cf1efb1b35cb7f80fa927e1d99a0d25ae
Emrys <emrys@openenergymonitor.org>
2020-04-02 16:02:41 +0100

    fixed problem with failed scans - not stopping adjusted debug title to include latest entry - tested with airoplane mode on to using native cordova offline&online events to save app.online added trace() function instead of repeating code in each function

--- WifiList/www/js/index.js
+++ WifiList/www/js/index.js
@@ -5,10 +5,13 @@ var app = {
     scan_retries: 0,
     max_retries: 4,
     scan_counter: 0,
+    redraw_counter: 0,
     accessPoints: [],
     currentSSID: '',
     repeat_seconds: 20,
     isWifiEnabled: false,
+    log_history: [],
+    max_history: 400,
     /**
      * wait for device to be ready
      */
@@ -20,16 +23,18 @@ var app = {
      * runs once cordova triggers the `deviceready` event
      */
     onDeviceReady: function() {
-        app.info(new Error().stack.match(/at (.*?) /)[1].replace('Object.','')+'()');
+        app.trace();
         
-        this.getWifiEnabled();
+        this.getWifiEnabled().then(state=>app.showWifiConnected(state));
         this.getConnectedSSID();
         this.showIpAddress();
         this.getWifiScanResults();
 
-        document.addEventListener('reload', this.reload);
-        document.addEventListener('connect', this.connect);
-
+        document.addEventListener('reload', this.reload, false);
+        document.addEventListener('connect', this.connect, false);
+        document.addEventListener("offline", ()=>{app.online=false; app.showWifiNetworks()}, false);
+        document.addEventListener("online", ()=>{app.online=true; app.showWifiNetworks()}, false);
+        
         // handle all clicks on document
         document.addEventListener('click', function (event) {
             // app.log(event.target);
@@ -44,22 +49,22 @@ var app = {
             }
         }, false);
         
-        // check which wifi network we're on every 10s
+        // check which wifi network we're on every 5s
         setInterval(function(){
             app.getConnectedSSID().then(app.showWifiNetworks());
-            app.getWifiEnabled();
-            app.showWifiConnected();
         }, 5000);
 
     },
+    /**
+     * returns promise with bool as parameter in sucessful then()
+     */
     getWifiEnabled: function() {
-        var state = WifiWizard2.isWifiEnabled();
-        app.isWifiEnabled = state;
-        app.info(state ? "WiFi is enabled": "WiFi is not available")
-        return state;
+        return WifiWizard2.isWifiEnabled();
     },
-    showWifiConnected: function() {
-        document.body.classList.toggle('online', app.isWifiEnabled);
+    showWifiConnected: function(state) {
+        if (typeof state !== "undefined") app.online = state;
+        app.info(app.online ? "WiFi is enabled": "WiFi is not available")
+        document.body.classList.toggle('online', app.online);
     },
     /**
      * get the list of access points
@@ -67,54 +72,56 @@ var app = {
      * on error retry after short delay
      */
     getWifiScanResults: function() {
-        app.info(new Error().stack.match(/at (.*?) /)[1].replace('Object.','')+'()');
-        
+        app.trace();
+
         app.hideReload();
         this.scan()
-        .then(function() {
-            app.showWifiNetworks();
-            app.scan_counter++;
-            // re-scan after delay
-            WifiWizard2.timeout(app.repeat_seconds*1000).then(function(){
-                app.info("Restarting scan " + app.scan_counter++)
-                app.getWifiScanResults();
-            });
-        })
-        .catch(function(reason){ 
-            // error running scan. clear auto refresh, wait and retry
-            app.error(app.scan_retries + ". " + reason);
-            WifiWizard2.timeout(2000).then(function(){
-                app.scan_retries++;
-                if (app.scan_retries > app.max_retries) {
-                    app.log("");
-                    app.info("Failed to scan. ;(");
-                    app.error("===================== END");
-                    app.scan_retries = 0;
-                    app.showReload();
-                    return;
-                } else {
-                    app.info("Restarting scan " + app.scan_counter++)
+            .then(function() {
+                app.showWifiNetworks();
+                app.scan_counter++;
+                app.scan_retries = 0;
+                app.log(`Scan ${app.scan_counter} complete. (${app.accessPoints.length} items)`);
+                
+                // re-scan after delay
+                WifiWizard2.timeout(app.repeat_seconds*1000).then(function() {
                     app.getWifiScanResults();
-                }
+                });
+            })
+            .catch(function(reason){ 
+                // error running scan. clear auto refresh, wait and retry
+                app.error(app.scan_retries + ". " + reason);
+                WifiWizard2.timeout(2000).then(function(){
+                    app.scan_retries++;
+                    if (app.scan_retries > app.max_retries) {
+                        app.log("");
+                        app.info("Failed to scan. ;(");
+                        app.error("============ CLICK FIND TO RESTART");
+                        app.scan_retries = 0;
+                        app.showReload();
+                        return;
+                    } else {
+                        app.info("Restarting scan " + app.scan_counter++)
+                        app.getWifiScanResults();
+                    }
+                });
             });
-        });
     },
     /**
      * display the list of networks to choose from
      * items in list are selectable to enable changing wifi connection
      */
     showWifiNetworks: function() {
-        app.info(new Error().stack.match(/at (.*?) /)[1].replace('Object.','')+'()');
+        app.trace();
+        app.redraw_counter++;
+        app.log(`${app.redraw_counter}. Re-drawing list (${app.accessPoints.length} items)`);
 
         const list = document.getElementById("list");
-        app.scan_retries = 0;
-        list.innerHTML = '';
-        app.log("showing the list...");
         const list_item = document.createElement("a");
+        list.innerHTML = '';
+        // TODO: update values not re-rendering whole list.
         
         // format data as list item for each entry
         app.accessPoints.forEach((ap, index) => {
-            app.log(index + ": " + ap.SSID);
             var item = list_item.cloneNode();
             var wps = ap.capabilities.match('WPS') ? ' WPS': '';
             var strength = "Unusable";
@@ -141,9 +148,9 @@ var app = {
             item.classList.toggle('active', ap.SSID === app.currentSSID);
             
             item.innerHTML = `<span>${title}<small class="badge text-muted">${wps}</small></span>
-                                <progress max="5" value="${rating}">
-                                    ${ap.level}dBm
-                                </progress>`;
+                              <progress max="5" value="${rating}">
+                                  ${ap.level}dBm
+                              </progress>`;
             list.append(item);
         });
     },
@@ -151,59 +158,81 @@ var app = {
     showIpAddress: function() {
         const container = document.getElementById("ip-address");
         WifiWizard2.getWifiIP()
-        .then(function(ip) {
-            app.info(`getWifiIP() response = ${ip}`);
-            if(container) container.innerText = `Connected as ${ip}`;
-        })
-        .catch(function(reason) { 
-            app.error(reason);
-            if(container) container.innerText = "Not connected";
-        });
+            .then(function(ip) {
+                app.info(`getWifiIP() response = ${ip}`);
+                if(container) container.innerText = `Connected as ${ip}`;
+            })
+            .catch(function(reason) {
+                app.error(reason);
+                if(container) container.innerText = "Not connected";
+            });
     },
 
     getConnectedSSID: function() {
-        app.info(new Error().stack.match(/at (.*?) /)[1].replace('Object.','')+'()');
+        app.trace();
         return WifiWizard2.getConnectedSSID()
-        .then(function(ssid) {
-            app.info("getConnectedSSID() response = " + ssid);
-            app.currentSSID = ssid;
-            return ssid;
-        })
-        .catch(function(reason){ 
-            app.error(reason);
-        });
+            .then(function(ssid) {
+                // app.info(`connected to "${ssid}"`);
+                app.currentSSID = ssid;
+                return ssid;
+            })
+            .catch(function(reason) { 
+                app.error(reason);
+            });
     },
     /**
      * request a scan and return a promise
      * throw errors if issue
      */
     scan: function() {
-        app.info(new Error().stack.match(/at (.*?) /)[1].replace('Object.','')+'()');
+        app.trace();
         app.log("Scanning...");
         app.startLoader('Searching');
         return WifiWizard2.scan()
-        .then(function(data) {
-            // app.log("JSON: " + JSON.stringify(data));
-            if (data.length === 0) throw "Empty Results";
-            app.log("Found: " + data.length);
-            app.accessPoints = data;
-            app.stopLoader();
-        })
-        .catch(function(reason){ 
-            // error
-            throw "Error getting list: " + reason;
-        })
+            .then(function(data) {
+                // app.log("JSON: " + JSON.stringify(data));
+                if (data.length === 0) throw "Empty Results";
+                app.log("Found: " + data.length);
+                app.accessPoints = data;
+                app.stopLoader();
+            })
+            .catch(function(reason){ 
+                // error
+                throw "Error getting list: " + reason;
+            })
     },
     log: function(text) {
-        const container = document.getElementById('output');
-        if (typeof container === "undefined") return;
-        // var isAtBottom = container.scrollTop === container.scrollHeight;
-        if(container) {
-            container.innerHTML += container.innerHTML.length === 0 ? text: "<br>" + text;
-            container.scrollTop = container.scrollHeight;
+        const container = document.getElementById("debug");
+        const output = container.querySelector("#output");
+        const last_entry = container.querySelector("summary .last-entry");
+
+        if (typeof container === "undefined"  || typeof output === "undefined") return;
+        var isAtBottom = output.scrollTop >= output.scrollHeight-output.clientHeight;
+        app.log_history = app.log_history.slice(-Math.abs(app.max_history));
+        app.log_history.push(text);
+        output.innerHTML = app.log_history.join("<br>");
+        // shift up if alredy at bottom
+        if(isAtBottom) {
+            output.scrollTop = output.scrollHeight;
+        }
+        // put the last line on the dropdown handle
+        var tmp = document.createElement("div");
+        tmp.innerHTML = app.log_history.slice(-1).join("");
+        if(last_entry) last_entry.innerText = tmp.innerText;
+    },
+    trace: function(depth) {
+        if(typeof depth === "undefined") depth = 1;
+        try{
+            var lines = new Error().stack.match(/at (.*?) /g).map(line => line.match(/at .*\.(.*?) /)[1]).slice(depth).reverse();
+            if (lines.length > 0) {
+                app.log("<em>"+lines.join("->") + "()</em>");
+            }
+        } catch (error) {
+            app.info("ERROR: unable to print trace: " + error.message);
         }
     },
     error: function(text) {
+        app.trace();
         app.log("<mark>ERROR: " + text + "</mark>");
     },
     info: function(text) {
@@ -242,14 +271,14 @@ var app = {
         app.log("Connection requested: " + ssid);
         
         WifiWizard2.connect(ssid, bindAll, password, algorithm, isHiddenSSID)
-        .then(function(){
-            app.info(`Connected succesfully to ${ssid}`);
-            app.getConnectedSSID();
-            app.showWifiNetworks();
-        })
-        .catch(function(reason){
-            app.error(reason);
-        })
+            .then(function(){
+                app.info(`Connected succesfully to ${ssid}`);
+                app.getConnectedSSID();
+                app.showWifiNetworks();
+            })
+            .catch(function(reason){
+                app.error(reason);
+            })
     }
 };
 
